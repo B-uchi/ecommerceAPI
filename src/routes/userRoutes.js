c28b1116e7323fb6657b3471008b7fb29b92c907
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-06-06 10:01:26 +0100

    added routes to api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
+import { signIn, signUp } from "../controllers/authController.js";
+import { verifyToken } from "../middleware/middleware.js";
+import userCacheMiddleware from "../middleware/userCacheMiddleware.js";
+import { addToCart } from "../controllers/userController.js";
 
 const router = Router();
 
+router.post("/signIn", signIn);
+router.post("/signUp", signUp);
 
+router.put("/addToCart", verifyToken, userCacheMiddleware, addToCart);
 
 export default router;
