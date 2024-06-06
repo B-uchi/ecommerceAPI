d7e6c2129474a557a4544f138d0f9ce3ccbbf60a
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-06-06 11:22:47 +0100

    added new routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { signIn, signUp } from "../controllers/authController.js";
 import { verifyToken } from "../middleware/middleware.js";
 import userCacheMiddleware from "../middleware/userCacheMiddleware.js";
-import { addToCart } from "../controllers/userController.js";
+import { addToCart, removeFromCart } from "../controllers/userController.js";
 
 const router = Router();
 
@@ -10,5 +10,6 @@ router.post("/signIn", signIn);
 router.post("/signUp", signUp);
 
 router.put("/addToCart", verifyToken, userCacheMiddleware, addToCart);
+router.put("/removeFromCart", verifyToken, userCacheMiddleware, removeFromCart);
 
 export default router;
